94d0d3f934c534389294ed70291de8b423b76821
Darrian Peck <130002007+darrianpeck@users.noreply.github.com>
2023-09-03 22:38:43 -0500

    Add files via upload

---
+++ gitTerms.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Terms</title>
+
+    <style>
+        body {
+            padding-left: 50px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php
+        echo "<h1>Git Related Terms</h1>";
+
+        echo "<p><b>Version Control Software:</b>Version control software is a software development practice that 
+        tracks and manages changes to code and other files while also keeping the history of those files. </p>";
+
+        echo "<p><b>Add:</b>If you would like to add a new or changed file to the working directory to the staging area then you would use
+        the git add commnand.</p>";
+
+        echo "<p><b>Commit:</b>Using the commit command will get a snapshot or screenshot of the current stage of the code/file. </p>";
+
+        echo "<p><b>Push:</b>Push is used to transfer files you've used commit command for from your local repository to a remote
+         repo.</p>";
+
+        echo "<p><b>Pull:</b>You can use the pull command to retrieve and download content from the remote repository, and then update 
+        the local repository to match that content.</p>";
+
+        echo "<p><b>Clone:</b>Using this command will copy an existing Git repository.</p>";
+    ?>
+
+</body>
+</html>
